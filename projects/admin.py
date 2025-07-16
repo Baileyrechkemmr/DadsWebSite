f63e367caed71e5689c857461554da3ec23c0251
Bailey Rechkemmer <benatzen@amazon.com>
2025-07-16 17:06:03 -0600

    Add complete Django website files - working baseline

---
+++ projects/admin.py
@@ -0,0 +1,66 @@
+from django.contrib import admin
+from .models import Year, Classes, Sword_img, Hotel, Blog, Sword_sales, BlogImages
+from django.utils.html import format_html
+# Register your models here.
+# for admin page
+
+# samm as in modles.py for name and what not
+
+
+@admin.register(Year)
+class YearAdmin(admin.ModelAdmin):
+    list_display = ['class_year']
+    search_fields = ['class_year']
+    list_filter = ['class_year']
+
+
+@admin.register(Classes)  # can also add a date time filter
+class ClassesAdmin(admin.ModelAdmin):
+    list_display = ['class_title']
+    search_fields = ['class_title']
+    list_filter = ['class_title']
+
+
+@admin.register(Sword_img)
+class Sword_imgAdmin(admin.ModelAdmin):
+    # def thumbnail(self, object):
+    #     return format_html('<img src="{}" width="40" />'.format(object.image.url))
+    
+    # list_display = ['item_number', 'thumbnail']
+    list_display = ['item_number']
+    search_fields = ['item_number']
+    list_filter = ['item_number']
+
+
+@admin.register(Sword_sales)
+class Sword_salesAdmin(admin.ModelAdmin):
+    # def thumbnail(self, object):
+    #     return format_html('<img src="{}" width="40" />'.format(object.image.url))
+
+    # list_display = ['item_number', 'thumbnail']
+    list_display = ['item_number']
+    search_fields = ['item_number']
+    list_filter = ['item_number']
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ['hotel_name', 'phone']
+    search_fields = ['hotel_name']
+    list_filter = ['hotel_name']
+
+
+class BlogImagesInline(admin.TabularInline):
+    model = Blog.images.through
+
+
+@admin.register(BlogImages)
+class BlogImagesAdmin(admin.ModelAdmin):
+    list_display = ['image']
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['date']
+    search_fields = ['date']
+    list_filter = ['date']
+    inlines = [BlogImagesInline]
---
+++ projects/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
+from django.utils.html import strip_tags
+from ckeditor.fields import RichTextField
+# Create your models here.
+#what u added at the admin page path
+
+
+
+class Year(models.Model):
+    title = models.CharField(default="year", max_length=4)
+    class_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Classes(models.Model):
+    class_title = models.CharField(max_length=250)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField(default="null")
+    class_slots = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.class_title
+# ask dad about price and if yes add price felid
+
+class Sword_img(models.Model):
+    item_number = models.IntegerField(default=0)
+    image = models.CharField(default="null", max_length=100)
+    # image = models.ImageField(upload_to='media/')
+    description = models.TextField(default="null")
+
+    def __str__(self):
+        return str(self.item_number)
+
+
+# at a later date have a felid possibly to use for the card for the travel and logins information 
+class Hotel(models.Model):
+    city_name = models.CharField(max_length=100)
+    hotel_name = models.CharField(max_length=250)
+    address = models.CharField(max_length=100)
+    description = models.TextField(default="null")
+    distance = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100, default=515-555-5555)
+
+    def __str__(self):
+        return self.hotel_name
+
+
+class Sword_sales(models.Model):
+    item_number = models.IntegerField(default=0)
+    image = models.CharField(default="null",max_length=100)
+    # image = models.ImageField(upload_to='media/')
+    description = models.TextField(default="null")
+    price = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.item_number)
+
+# admin feild for blog posts
+
+class BlogImages(models.Model):
+    image = models.CharField(default="null", max_length=100)
+
+
+class Blog(models.Model):
+    date = models.DateField(auto_now_add=True)
+    description = RichTextField(default="null")
+    images = models.ManyToManyField(BlogImages, blank=True)
+
+    @property
+    def stripped_rich_field(self):
+        return strip_tags(self.description)
+    
+    
+    def __str__(self):
+        return str(self.date)
